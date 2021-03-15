b76c1fe01378764c70a7f744d401abfe51becbd2
Arvind <Arvind@LAPTOP-A3ONQS5O>
2021-03-15 13:58:01 +0530

    3rd time

--- test1/src/main/java/com/crm/objectutility/Contact.java
+++ test1/src/main/java/com/crm/objectutility/Contact.java
@@ -2,7 +2,7 @@ package com.crm.objectutility;
 
 public class Contact {
 
-	public void contact(String name, String pasword)
+	public void contact(String name, String pasword,String path)
 	{
 		
 	}
