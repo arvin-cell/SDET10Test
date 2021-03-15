90e84762dc8f934f9843a764494c9f17b3df6c56
Arvind <Arvind@LAPTOP-A3ONQS5O>
2021-03-15 12:57:09 +0530

    1st test

---
+++ test1/src/test/java/com/Test/DemoTest.java
@@ -0,0 +1,26 @@
+package com.Test;
+
+import org.testng.annotations.Test;
+
+public class DemoTest
+{
+	@Test
+	public void createtest()
+	{
+		System.out.println("Account created");
+	}
+	
+	@Test
+	public void deletetest() 
+	{
+
+		System.out.println("Account deleted");
+	}
+	@Test
+	public void modifytest() 
+	{
+
+		System.out.println("Account modify");
+	}
+
+}
